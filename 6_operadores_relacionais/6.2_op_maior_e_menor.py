b85170398779a064aae32b567b1808ee1c8f3a41
Juliano Silva <julianoqm@gmail.com>
2022-12-30 16:53:12 -0300

    6.2_op_maior_e_menor
    Operadores de comparação Maior que e menor que

--- 6_operadores_relacionais/6.2_op_maior_e_menor.py
+++ 6_operadores_relacionais/6.2_op_maior_e_menor.py
@@ -4,3 +4,11 @@ O sinal de > verifica se um valor é maior que o outro;
 Já o < verifica se um valor é menor que o outro;
 O resultado será um Boolean a comparação entre dois valores;
 '''
+a = 10
+b = 5
+c = 20
+
+print(a > b)
+print(a > c)
+print(a < b)
+print(a < c)
