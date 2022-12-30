4e5350f1474a6be27fb5294637c5c8a9b711ff88
Juliano Silva <julianoqm@gmail.com>
2022-12-30 15:44:53 -0300

    Dado_Bolleano
    Aula 5 - tipo de dados Bolleanos (verdadeiro ou falso)

---
+++ 5_booleanos/app.py
@@ -0,0 +1,12 @@
+'''
+O tipo de daod boolean se resume e TRUE e FALSE, ou seja, verdadeiro e falso
+Podemos criar variáveis com estes valores;
+E eles serão úteis mais a frente, quando criamos programas que tem condições de comparação
+'''
+verdadeiro = True
+print(verdadeiro)
+print(type(verdadeiro))
+
+falso = False
+print(falso)
+print(type(falso))
