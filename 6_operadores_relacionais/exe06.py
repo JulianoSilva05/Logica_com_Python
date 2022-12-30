308229140bc0abca49dca6d0a2f71dddad0aa35e
Juliano Silva <julianoqm@gmail.com>
2022-12-30 17:23:21 -0300

    Exercicios_06_operadores_comparacao
    Exercícios sobre operadores de comparação

---
+++ 6_operadores_relacionais/exe06.py
@@ -0,0 +1,29 @@
+'''
+Crie as variáveis: a=5, b=3, c=10, d=2;
+Realize as seguintes comparações em teste de mesa:
+a > b
+c == a
+d != a
+a <= c
+b > c
+a != c
+d >= a
+d != b
+d < c
+b >= a
+'''
+a=5 
+b=3
+c=10
+d=2
+
+a > b #resultado
+c == a #resultado
+d != a #resultado
+a <= c #resultado
+b > c #resultado
+a != c #resultado
+d >= a #resultado
+d != b #resultado
+d < c #resultado
+b >= a #resultado
