dbcca6055daa3dbc145ebc09bef1ed7a25bc1dae
JulianoSilva05 <57194781+JulianoSilva05@users.noreply.github.com>
2023-01-10 17:52:48 -0300

    Op_logicos
    Operadores lógicos

---
+++ 7_op_logicos/not.py
@@ -0,0 +1,13 @@
+#Operador NOT
+'''
+O operador not vai inverter um booleano;
+Ou seja, quando criamos uma comparação que resulta em booleano, podemos inverter com not;
+True se torna False e False se torna True;
+'''
+verdadeiro = True
+falso = False
+
+print(not verdadeiro)
+print(not falso)
+
+print(not(5 > 2))
