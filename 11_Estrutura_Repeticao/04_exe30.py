f58edc57943017d2cf6aa8f633d8af38ff03b601
Juliano Silva <julianoqm@gmail.com>
2023-01-26 16:25:21 -0300

    Create_exe30

---
+++ 11_Estrutura_Repeticao/04_exe30.py
@@ -0,0 +1,9 @@
+#Exercicio 30
+'''
+Faça um programa para escrever a contagem regressiva do lançamento de um foguete. 
+O programa deve imprimir 10, 9, 8, ..., 1, 0 e Fogo! na tela.
+'''
+cont = 10
+while cont >= 0 :
+    print(cont)
+    cont = cont - 1
